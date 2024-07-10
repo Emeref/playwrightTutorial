b26076e97415278af169c454f44f7151bda67e77
Kobrad <konrad.rafalko@gmail.com>
2024-07-10 13:08:36 +0200

    update

--- tests_ui_layout/conftest.py
+++ tests_ui_layout/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from playwright.sync_api import Playwright
-
+import os
 import secret_config2
 
 
@@ -23,7 +23,7 @@ def login_set_up(set_up):
     page.locator("[data-test=\"username\"]").click()
     page.locator("[data-test=\"username\"]").fill("standard_user")
     page.locator("[data-test=\"username\"]").press("Tab")
-    page.locator("[data-test=\"password\"]").fill([os.environ['PASSWORD'])
+    page.locator("[data-test=\"password\"]").fill(os.environ['PASSWORD'])
     page.locator("[data-test=\"password\"]").press("Tab")
     page.get_by_role("img").click()
     page.locator(".login_logo").click(timeout=1000)
